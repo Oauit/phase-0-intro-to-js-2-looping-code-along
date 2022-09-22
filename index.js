63274145f21d6ac94c0eeb2abb93eb97058b1050
oauit <aidanjade.ward@gmail.com>
2022-09-22 10:35:50 -0600

    commit tree 3a5a00b9a8d2ee7a573d1b24004a348ec4b5319d

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards, event) {
+    const messages = []
+    for (let i = 0; i <cards.length; i++) {
+        messages.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return messages;
+}
+
+writeCards(cards);
+
+function countDown(){
+let countDown = 10;
+while (countDown >= 0) {
+    console.log(countDown--)
+    
+}
+return countDown;
+}
